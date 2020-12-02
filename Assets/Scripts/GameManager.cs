d0d5ee6a70ac64c5b44ebb16cc2b9fda8d8749c5
KreconyMakaron <KreconyMakaron@users.noreply.github.com>
2020-12-02 21:13:49 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+    bool gamePaused = false;
+    public Text pauseText;
+
+    void Start()
+    {
+
+       if(gameManager == null)
+       {
+            gameManager = this;
+       }
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Debug.Log("Pause Game");
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Resume Game");
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+}
